3c43efedf553cb28621117b8ed54d57a35954296
RoyalNeverGiveUp <767664872@qq.com>
2016-11-30 10:54:06 +0800

    登录页面的修改

--- MenCloths/app/src/main/java/com/men_cloths/mainContent/ActivityLogin.java
+++ MenCloths/app/src/main/java/com/men_cloths/mainContent/ActivityLogin.java
@@ -2,11 +2,19 @@ package com.men_cloths.mainContent;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.Gravity;
 import android.view.View;
 
 
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.men_cloths.R;
@@ -22,8 +30,10 @@ import java.net.URL;
 public class ActivityLogin extends Activity {
     TextView login;
     TextView register;
-    ImageView tencent,wechat,sina,taobao;
-
+    ImageView tencent,wechat,sina,taobao,showpassword,camera;
+    EditText phonenumb,password;
+    int i=0;
+    PopupWindow popupWindow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +45,13 @@ public class ActivityLogin extends Activity {
         wechat = (ImageView) findViewById(R.id.wechat);
         sina = (ImageView) findViewById(R.id.sina);
         taobao = (ImageView) findViewById(R.id.taobao);
+        showpassword=(ImageView)findViewById(R.id.img_login_showpassword);
+        camera=(ImageView)findViewById(R.id.img_login_cawera);
+        phonenumb=(EditText)findViewById(R.id.edit_login_phonenumb);
+        password=(EditText)findViewById(R.id.edit_login_password);
         //绑定监听事件
+        showpassword.setOnClickListener(onClickListener);
+        camera.setOnClickListener(onClickListener);
         login.setOnClickListener(onClickListener);
         register.setOnClickListener(onClickListener);
         tencent.setOnClickListener(onClickListener);
@@ -73,7 +89,37 @@ public class ActivityLogin extends Activity {
 //                        e.printStackTrace();
 //                    }
                     break;
+                case R.id.img_login_cawera:
+                    CreatePopuwindow();
+                    Button camera=(Button)findViewById(R.id.btn_camera);
+                    Button dismiss=(Button)findViewById(R.id.btn_longin_dismiss);
+                    if(dismiss!=null){
+                        dismiss.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                popupWindow.dismiss();
+                            }
+                        });
+                    }
+
+                    break;
+                case R.id.img_login_showpassword:
+                    i++;
+                    if(i%2==1){
+                        password.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    }else{
+                        password.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    }
+                    break;
             }
         }
     };
+    public void CreatePopuwindow(){
+        popupWindow=new PopupWindow(getLayoutInflater().inflate(R.layout.popuwindow_login_camera,null), LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        ColorDrawable dw = new ColorDrawable(0xb0000000);
+        popupWindow.setBackgroundDrawable(dw);
+        popupWindow.setOutsideTouchable(true);
+        popupWindow.showAtLocation(camera, Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+
+    }
 }
