6d13dd6b9be47ae1cb45a3d032c753be50652d13
zhangwen <you@example.com>
2016-11-30 10:47:15 +0800

    潮流详情，单品详情页面修改

--- MenCloths/app/src/main/java/com/men_cloths/mainContent/ThreadInfo.java
+++ MenCloths/app/src/main/java/com/men_cloths/mainContent/ThreadInfo.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.men_cloths.R;
 import com.men_cloths.adapter.AdapterforAll;
@@ -19,14 +20,20 @@ import java.util.List;
 
 public class ThreadInfo extends Activity{
 
-    ListView listView;
-    List list;
-    LayoutInflater inflater;
+   private ListView listView;
+   private List list;
+   private LayoutInflater inflater;
+   private ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.trend_write_zw);
+        setContentView(R.layout.trend_zw);
         initView();
+        View view=inflater.inflate(R.layout.trend_write_zw,null);
+
+        listView.addHeaderView(view);
+        createData();
+        listSetAdapter();
 
 
     }
@@ -35,20 +42,54 @@ public class ThreadInfo extends Activity{
         listView= (ListView) findViewById(R.id.list_view_for_trend);
         list=new ArrayList();
         inflater=LayoutInflater.from(this);
+        back= (ImageView) findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     public void listSetAdapter(){
         listView.setAdapter(new AdapterforAll(this, list) {
             @Override
             public View setView(int position, View convertView) {
+                Holder holder=null;
+                if(convertView==null){
+                    convertView=inflater.inflate(R.layout.trend_write_zhangwen_item,null);
+                    holder=new Holder();
+                    holder.imageView= (ImageView) convertView.findViewById(R.id.zhangwen_suai);
+                    holder.title= (TextView) convertView.findViewById(R.id.title);
+                    holder.contents= (TextView) convertView.findViewById(R.id.content);
+                    holder.logo= (TextView) convertView.findViewById(R.id.logo);
+                    holder.goumai= (TextView) convertView.findViewById(R.id.goumai);
+                    convertView.setTag(holder);
+                }else {
+                    holder= (Holder) convertView.getTag();
+                }
 
 
-
-                return null;
+                return convertView;
             }
         });
     }
+    /**
+     * title        TextView
+     content        TextView
+     zhangwen_suai ImageView
+     logo      TextView
+     goumai    TextView
+     * goumai
+     * */
     private class Holder{
         ImageView imageView;
+        TextView title,contents,logo,goumai;
     }
+
+
+    public void createData(){
+        list.add(1);
+    }
+
 }
